561413e326735c68cf8e1e63cdc8e56438ce171c
matt94856 <129871867+matt94856@users.noreply.github.com>
2024-08-04 20:17:08 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Function to add a product to the cart
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
+
+// You can add more JavaScript functions here for additional functionality
